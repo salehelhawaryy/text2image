52e6f03731b99e91f0951e17cbc2c163401c9168
salehelhawaryy <salehmohamad210@gmail.com>
2024-04-05 22:26:55 +0200

    test

--- model.py
+++ model.py
@@ -230,15 +230,24 @@ class NetD(nn.Module):
         self.COND_DNET = D_GET_LOGITS(ndf)
 
     def forward(self, x):
-
+        print('')
+        print('')
         out = self.conv_img(x)
+        print(out.shape)
         out = self.block0(out)
+        print(out.shape)
         out = self.block1(out)
+        print(out.shape)
         out = self.block2(out)
+        print(out.shape)
         out = self.block3(out)
+        print(out.shape)
         out = self.block4(out)
+        print(out.shape)
         out = self.block5(out)
         print(out.shape)
+        print('')
+        print('')
 
         return out
 

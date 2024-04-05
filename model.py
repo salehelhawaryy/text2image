0e7c97d2b5fa7c5061a1bef02306f55c790aaae5
salehelhawaryy <salehmohamad210@gmail.com>
2024-04-05 21:18:28 +0200

    testing

--- model.py
+++ model.py
@@ -209,6 +209,7 @@ class D_GET_LOGITS(nn.Module):
 
         y = y.view(-1, 256, 1, 1)
         y = y.repeat(1, 1, 4, 4)
+        print(out.shape, y.shape)
         h_c_code = torch.cat((out, y), 1)
         out = self.joint_conv(h_c_code)
         return out

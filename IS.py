4b8ee3332a9bfeb66ddc2631d65d53711f683037
salehelhawaryy <salehmohamad210@gmail.com>
2024-04-06 18:04:03 +0200

    bug fix

--- IS.py
+++ IS.py
@@ -181,7 +181,7 @@ def _init_inception():
                 print(shape)
                 for s in shape:
                     print(s)
-                shape = [s.value for s in shape]
+                shape = [s.value if s is not None else None for s in shape]
                 new_shape = []
                 for j, s in enumerate(shape):
                     if s == 1 and j == 0:
